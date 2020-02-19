e53df2aa609dae43c307287e5366b3dec3ab6b83
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-19 16:20:49 -0700

    added modal config to change image based on card that was clicked

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ modal.addEventListener("click", function() {
 });
 
 function openModal() {
-  console.log("Open Modal");
   modal.classList.remove("d-none");
 }
+
+var slideIndex = 1;
+
+const currentSlide = num => {
+  let dots = document.getElementsByClassName("card-image");
+  let url = dots[num].src;
+  modal.querySelector("img").src = url;
+};
